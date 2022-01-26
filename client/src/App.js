412d5b4836be40f32318708d9f08a0aa4830d5b1
Raktim Shrestha <raktim@rumsan.com>
2022-01-26 21:55:44 +0545

    remove react toast and add swal

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { ToastProvider } from 'react-toast-notifications';
 import TodoList from './modules/todos';
 
 export default function App() {
   return (
     <>
-      <ToastProvider>
-        <TodoList />
-      </ToastProvider>
+      <TodoList />
     </>
   );
 }
