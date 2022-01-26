98faab57666854c48ea7cd2f9cef640a68232a0b
Raktim Shrestha <raktim@rumsan.com>
2022-01-26 14:53:14 +0545

    enabling cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -16,6 +17,7 @@ const routeManager = require('./routes');
 
 const swaggerDoc = require('./documentation');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
